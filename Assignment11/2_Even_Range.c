67e1d497cc574bf86baf45ebddcc8f99a472515b
Pratiksha Ghadage <pratikshaghadage22@gmail.com>
2026-07-21 00:44:54 +0530

    Add files via upload

---
+++ Assignment11/2_Even_Range.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void RangeDisplayEven(int iStart, int iEnd)
+{
+    if(iStart < 0)
+    {
+        iStart = - iStart;
+    }
+    if(iStart > iEnd)
+    {
+        printf("Invalid Range");
+    }
+
+    for(iStart; iStart <= iEnd; iStart++)
+    {
+        if(iStart % 2 ==0)
+        {
+            printf("%d  ",iStart);
+        }
+        
+    }
+}
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter a starting point: ");
+    scanf("%d", &iValue1);
+
+    printf("Enter a ending point: ");
+    scanf("%d", &iValue2);
+
+    RangeDisplayEven(iValue1, iValue2);
+
+    return 0;
+}
