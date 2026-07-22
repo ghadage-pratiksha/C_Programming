77c7f4f5650732e2486e2a9fb9cfb3328dda66a9
Pratiksha Ghadage <pratikshaghadage22@gmail.com>
2026-07-23 01:04:17 +0530

    Update 2_NumberToWord.c

--- Assignment8/2_NumberToWord.c
+++ Assignment8/2_NumberToWord.c
@@ -12,31 +12,40 @@ void Display(int iNo)
         case 0:
             printf("Zero");
             break;
+        case -1:
         case 1:
             printf("One");
             break;
         case 2:
+        case -2:
             printf("Two");
             break;
         case 3:
+        case -3:
             printf("Three");
             break;
         case 4:
+        case -4:
             printf("Four");
             break;
         case 5:
+        case -5:
             printf("Five");
             break;
         case 6:
+        case -6:
             printf("Six");
             break;
         case 7:
+        case -7:
             printf("Seven");
             break;
         case 8:
+        case -8:
             printf("Eight");
             break;
         case 9:
+        case -9:
             printf("Nine");
             break;
         default:
