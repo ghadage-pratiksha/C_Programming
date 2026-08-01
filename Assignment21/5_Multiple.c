7931b816e4e90b3b74f17f6b9081f55f8ac847b0
Pratiksha Ghadage <pratikshaghadage22@gmail.com>
2026-08-02 02:41:40 +0530

    Add files via upload

---
+++ Assignment21/5_Multiple.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include<stdlib.h>
+void Display(int Arr[] , int iLength)
+{
+    int i =0;
+    for(i=0; i<iLength; i++)
+    {
+        if(Arr[i] % 11 == 0)
+        {
+            printf("\nThe number divisible by 5 and even is :%d\n",Arr[i]);
+        }
+    }
+}
+int main()
+{
+    int iSize=0;
+    int iCnt=0;
+    int *p=NULL;
+    
+    printf("Enter numbers of elements:");
+    scanf("%d",&iSize);
+    
+    p=(int *)malloc(iSize * sizeof(int));
+    
+    if(p == NULL)
+    {
+        printf("Unable to get memory");
+        return -1;
+    }
+    
+    printf("Enter %d elements\n",iSize);
+    
+    for(iCnt=0; iCnt < iSize ; iCnt++)
+    {
+        printf("Enter element %d:", iCnt + 1);
+        scanf("%d",&p[iCnt]);
+    }
+    
+    printf("You enterd:\n");
+    for(iCnt=0; iCnt < iSize; iCnt++)
+    {
+        printf("%d\t ",p[iCnt]);
+    }
+    Display(p, iSize);
+    free(p);
+    return 0;
+}
