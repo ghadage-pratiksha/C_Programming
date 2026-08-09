87a447077f8521722d97ba7e9fc39c9fca9504a0
Pratiksha Ghadage <pratikshaghadage22@gmail.com>
2026-08-09 11:13:37 +0530

    Add files via upload

---
+++ Assignment25/2_Capital.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+
+
+BOOL ChkCapital(char ch)
+{
+    if(ch >= 'A' && ch <= 'Z') 
+    {
+        return TRUE;
+    }
+    else
+    {
+        return FALSE;
+    }
+}
+int main() 
+{
+    char cValue='\0';
+    BOOL bRet = FALSE;
+    
+    printf("Enter the character:");
+    scanf("%c",&cValue);
+    
+    bRet = ChkCapital(cValue);
+    
+    if(bRet == TRUE)
+    {
+        printf("It is Captial Character");
+    }
+    else
+    {
+        printf("it is not  Capital character");
+    }
+    return 0;
+}
